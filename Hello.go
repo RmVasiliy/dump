aa458846c5d55a20d6988ba333eb36eeff8bccc6
RmVasiliy <rm.vasiliy@gmail.com>
2021-02-09 16:27:50 +0300

    123

--- Hello.go
+++ Hello.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1235")
-	fmt.Println("5555")
-	fmt.Println("11111")
-	fmt.Println("324234")
-	fmt.Println("166666")
+	a := 14545
+	p := &a
+	fmt.Println("%x - %d", p, *p)
 }
